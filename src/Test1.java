64b5069fbe2d67e2ec0d6ae3291f487bdbd75316
HayLon <466937439@qq.com>
2020-02-26 02:30:40 +0800

    GIT COMMIT 1

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test git 2");
+        System.out.println("test git 2");
+        System.out.println("test git 2");
+        Test1.test();
+    }
+    public static void test(){
+        System.out.println("test git test");
+    }
+}
